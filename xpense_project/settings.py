01a2c4f2d2822cdb0a49995478e0b1a788d0fabb
mahianmahin <80479590+mahianmahin@users.noreply.github.com>
2022-01-31 00:00:03 +0600

    progress

--- xpense_project/settings.py
+++ xpense_project/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-1&jhvvj*iopktl0z8t)9!oy-wwy8!f()*zu=t=0rgcjojzw404
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
